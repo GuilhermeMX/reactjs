69e58dc0a8a946356e68f5a941be136d06175981
Gui <guilhermenen15@gmail.com>
2022-10-22 04:58:35 -0300

    update css

--- react-app/fundamentos-react/src/components/Header.jsx
+++ react-app/fundamentos-react/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/logo.svg'
+
 export function Header () {
   return (
     <header className={styles.header}>
-      <strong>My Feed</strong>
+      <img src={igniteLogo} alt="Logotipo" />
     </header>
   );
 }
