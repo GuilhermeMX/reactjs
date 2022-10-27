ac9a6f9b1e721c3e1f6a66de40a6915dd9409035
Gui <guilhermenen15@gmail.com>
2022-10-27 20:59:31 -0300

    Create sidebar components

--- react-app/fundamentos-react/src/App.jsx
+++ react-app/fundamentos-react/src/App.jsx
@@ -1,17 +1,30 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
-
       <Header />
-      <Post
-        author="Guilherme Xavier"
-        content="O brabo da cultura e tudo mais"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+
+        <main>
+          <Post
+            author="Guilherme Xavier"
+            content="O brabo da cultura e tudo mais"
+          />
+          <Post
+            author="Guilherme Xavier"
+            content="O brabo da cultura e tudo mais"
+          />
+        </main>
+      </div>
     </div>
   )
 }
