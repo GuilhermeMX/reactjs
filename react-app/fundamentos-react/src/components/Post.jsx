062e156ea3e420b8356b4050f8987f6aef9be42a
Gui <guilhermenen15@gmail.com>
2022-11-21 11:43:10 -0300

    create post.styles

--- react-app/fundamentos-react/src/components/Post.jsx
+++ react-app/fundamentos-react/src/components/Post.jsx
@@ -11,7 +11,16 @@ export function Post() {
             <span>Software Developer</span>
           </div>
         </div>
+
+        <time title="21 de novembro as 11:02" dateTime="2022-11-21 11:03:30">Publicado há 1h</time>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galera!!</p>
+        <p>Acabei de subir mais um projeto no meu portifólio</p>
+        <p><a href="https://github.com/GuilhermeMX/">https://github.com/GuilhermeMX/</a></p>
+        <p><a href="#">#nlw #rocketseat</a></p>
+      </div>
     </article>
   )
 }
