cbd7f3ef7550c8cb90e0bac833212b5f32bf4e7c
Gui <guilhermenen15@gmail.com>
2023-01-12 12:16:39 -0300

    create comment section

---
+++ react-app/fundamentos-react/src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/guilhermeMX.png" alt="" />
+    
+      <div className={styles.commentBox}>
+    
+      </div>
+    </div>
+  )
+}
