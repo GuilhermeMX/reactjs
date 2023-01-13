f49949ea39cc0ac9d0e79297fe4555411ff7431b
Gui <guilhermenen15@gmail.com>
2023-01-13 10:54:53 -0300

    create comment section

--- react-app/fundamentos-react/src/components/Comment.jsx
+++ react-app/fundamentos-react/src/components/Comment.jsx
@@ -1,3 +1,4 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment() {
@@ -6,8 +7,30 @@ export function Comment() {
       <img src="https://github.com/guilhermeMX.png" alt="" />
     
       <div className={styles.commentBox}>
-    
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Guilherme Xavier</strong>
+              <time title="21 de novembro as 11:02" dateTime="2022-11-21 11:03:30">Publicado há 1h</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
       </div>
+
+      
     </div>
   )
 }
