b4d9bb225305302e41617e71a60e5266b8f68d29
Gui <guilhermenen15@gmail.com>
2023-01-30 11:55:56 -0300

    create avatar component

--- react-app/fundamentos-react/src/components/Sidebar.jsx
+++ react-app/fundamentos-react/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,11 +12,8 @@ export function Sidebar() {
       /> 
 
       <div className={styles.profile}>
-        <img 
-          className={styles.avatar}
-          src="https://github.com/GuilhermeMX.png" 
-        />
-        
+        <Avatar src="https://github.com/GuilhermeMX.png"/>
+
         <strong>Guilherme M. Xavier</strong>
         <span>Software Developer</span>
       </div>    
